12f0fad68cebc49597e1aaaa63ba2905c8fff579
heidyvl <heidy.villarreal@spsmail.cuny.edu>
2015-05-03 02:49:29 +0000

    picklecache

--- picklecache.py
+++ picklecache.py
@@ -5,33 +5,48 @@
 import os
 import pickle
 
-class PickleCache():
+
+class PickleCache(object):
+    """Class Docstring"""
     def __init__(self, file_path='datastore.pkl', autosync=False):
+        """Constructor"""
         self.__file_path = file_path
         self.__data = {}
         self.autosync = autosync
-        
-        
+        self.load()
+
     def __setitem__(self, key, value):
-        
+        """SetItem function"""
         self.__data[key] = value
-        
+
     def __len__(self):
+        """Gets len"""
         return len(self.__data)
 
     def __getitem__(self, key):
+        """Get item function"""
+        if self.autosync is True:
+            self.flush()
+            self.__delitem__(key)
         try:
             return self.__data[key]
         except (TypeError, KeyError):
-            raise KeyError
+            raise
 
     def __delitem__(self, key):
+        """Deletes Item"""
         del self.__data[key]
 
     def load(self):
+        """Loads item"""
         if (os.path.exists(self.__file_path)
-            and os.path.getsize(self.__file_path) != 0):
-            self.__data = pickle.load(self.__file_path)
-            close(self.__file_path)
-            
-            
+           and os.path.getsize(self.__file_path) != 0):
+            fhandler = open(self.__file_path, 'r')
+            self.__data = pickle.load(fhandler)
+            fhandler.close()
+
+    def flush(self):
+        """Flush function"""
+        fhandler = open(self.__file_path, 'wb')
+        pickle.dump(self.__data, fhandler)
+        fhandler.close()
